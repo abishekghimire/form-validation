21d728573f5974b9642343574cea2e222ba1f852
abishekghimire <abishekghimire220.com>
2024-09-21 22:03:07 +0545

    added validation and real time feedback for DOB

--- form/js/dobValidation.js
+++ form/js/dobValidation.js
@@ -1,1 +1,51 @@
 const dobRegex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+
+export function dobValidation(
+  dobField,
+  ageLimitFeedback,
+  invalidDateFeedback,
+  futureDateFeedback
+) {
+  dobField.addEventListener("input", () => {
+    const dob = new Date(dobField.value);
+    const today = new Date();
+    const age = today.getFullYear() - dob.getFullYear();
+    const monthDifference = today.getMonth() - dob.getMonth();
+
+    invalidDateFeedback.style.display = "none";
+    ageLimitFeedback.style.display = "none";
+    futureDateFeedback.style.display = "none";
+
+    if (!dobRegex.test(dobField.value)) {
+      invalidDateFeedback.style.display = "block";
+    } else {
+      if (dob > today) {
+        futureDateFeedback.style.display = "block";
+      } else {
+        if (
+          age < 16 ||
+          (age === 16 && monthDifference < 0) ||
+          (age === 16 &&
+            monthDifference === 0 &&
+            today.getDate() < dob.getDate())
+        ) {
+          ageLimitFeedback.style.display = "block";
+        }
+      }
+    }
+  });
+
+  const validateDOB = (event) => {
+    const dob = dobField.value;
+
+    if (!dobRegex.test(dob)) {
+      event.preventDefault();
+      // showToast(invalidMsg, "orange");
+    }
+  };
+  const signupForm = document.getElementById("signupForm");
+
+  if (signupForm) {
+    signupForm.addEventListener("submit", validateDOB);
+  }
+}
--- form/js/main.js
+++ form/js/main.js
@@ -1,8 +1,15 @@
+import { dobValidation } from "./dobValidation.js";
 import { emailValidation } from "./emailValidation.js";
 import { showHidePassword } from "./passwordShowHide.js";
 import { passwordValidation } from "./passwordValidation.js";
 const emailField = document.querySelector(".form input[type = 'email']");
 const emailFeedback = document.querySelector(".email-feedback");
+
+const dobField = document.querySelector(".form input[type = 'text']");
+const ageLimitFeedback = document.querySelector(".age-limit-feedback");
+const invalidDateFeedback = document.querySelector(".invalid-date-feedback");
+const futureDateFeedback = document.querySelector(".future-date-feedback");
+
 const passwordField = document.querySelector(".form input[type= 'password']");
 const toggleBtn = document.querySelector(".form .field i");
 const passwordLengthFeedback = document.querySelector(
@@ -13,3 +20,9 @@ const passwordSetFeedback = document.querySelector(".password-set-feedback");
 emailValidation(emailField, emailFeedback);
 showHidePassword(passwordField, toggleBtn);
 passwordValidation(passwordField, passwordLengthFeedback, passwordSetFeedback);
+dobValidation(
+  dobField,
+  ageLimitFeedback,
+  invalidDateFeedback,
+  futureDateFeedback
+);
